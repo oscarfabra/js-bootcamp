5769bca02f20708f2a51342029e1faa925dd25eb
Oscar Fabra <oscarfabra@gmail.com>
2020-07-13 23:34:45 -0500

    Adds solutions to last 3 challenges of Section 5 - Objects

--- functions/grade-calc.js
+++ functions/grade-calc.js
@@ -1,5 +1,6 @@
-let getGradeMessage = function (score, totalScore = 100) {
-  let percent = (score / totalScore) * 100
+const getGradeMessage = function (score, totalScore = 100) {
+  const percent = (score / totalScore) * 100
+  let grade = ''
   if (percent >= 90 && percent <= 100) {
     grade = 'A'
   } else if (percent >= 80) {
@@ -11,9 +12,9 @@ let getGradeMessage = function (score, totalScore = 100) {
   } else {
     grade = 'F'
   }
-  let preposition = (grade === 'A' || grade === 'F') ? 'an' : 'a'
+  const preposition = (grade === 'A' || grade === 'F') ? 'an' : 'a'
   return `You got ${preposition} ${grade} (${percent}%)!`
 }
 
-let message = getGradeMessage(19, 20);
+const message = getGradeMessage(19, 20);
 console.log(message)
---
+++ objects/const.js
@@ -0,0 +1,8 @@
+const person = {
+  age: 35
+}
+
+person.age ++
+// person = {}
+
+console.log(person)
